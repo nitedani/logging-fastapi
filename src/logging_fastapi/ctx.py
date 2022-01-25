be4d54d111f78311cf52d8fa077179bfdb00202e
nitedani <nitedani@gmail.com>
2022-01-25 19:58:11 +0100

    test

--- src/logging_fastapi/ctx.py
+++ src/logging_fastapi/ctx.py
@@ -1,5 +1,5 @@
 from contextvars import ContextVar
 
 span_ctx = ContextVar("span")
-request_id_ctx = ContextVar("requestId")
 error_ctx = ContextVar("error")
+request_id_ctx = ContextVar("requestId")
